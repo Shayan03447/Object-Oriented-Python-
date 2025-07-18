62c59d118c4b0538c4b23050971defee9f0463cd
Shayan <shanich03447@gmail.com>
2025-07-19 01:22:44 +0500

    project complete

--- OOPs_project.py
+++ OOPs_project.py
@@ -13,15 +13,54 @@ class chatbook:
                          4. press 4 to message a friend
                          5. press any other key to exit""")
         if user_input=="1":
-            pass
+            self.signup()
         elif user_input=="2":
-            pass
+            self.signin()
         elif user_input=="3":
-            pass
+            self.my_post()
         elif user_input=="4":
             pass
         else:
             exit
+
+    def signup(self):
+        email=input("Enter your email here ->")
+        pwd=input("Setup your password here ->")
+        self.username=email
+        self.password=pwd
+        print("You have signed up successfully !!")
+        print("\n")
+        self.menu() 
+
+    def signin(self):
+        if self.username=="" and self.password=="":
+            print("please signup first by pressing 1 in the main menu")
+        else:
+            username=input("enter your email/username here ->")
+            pwd=input("Enter your password here ->")
+            if self.username==username and self.password==pwd:
+                print("You have signed in successfully !!")
+                self.loggedin=True
+            else:
+                print("please enter correct credential")
+        print("\n")
+        self.menu()
+
+    def my_post(self):
+        if self.loggedin==True:
+            txt=input("Enter your message here ->")
+            print(f"following content has been posted -> {txt}")
+        else:
+            print("You need to signin first to post something....")
+        print("\n")
+        self.menu()
+
+        
+
+    
+
+
+
 
 object=chatbook()
 
--- rough.py
+++ rough.py
@@ -1,8 +1,10 @@
-int=123
-str='shayan'
-float=12.3
-lst=[1,2,3]
+# int=123
+# str='shayan'
+# float=12.3
+# lst=[1,2,3]
 
-print(type(str))
-lst.clear()
-print(lst)
+# print(type(str))
+# lst.clear()
+# print(lst)
+from OOPs_project import chatbook
+user1=chatbook()
